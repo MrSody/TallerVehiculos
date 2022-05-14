d86499174192e54cfc06cd94ed5494bc5e1e4197
MrSody <kristian_9411@hotmail.com>
2022-05-14 14:46:17 -0500

    add: controller ciudades

---
+++ TallerVehiculos/Controllers/CiudadesController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TallerVehiculos.Data;
+using TallerVehiculos.Models;
+
+namespace TallerVehiculos.Controllers
+{
+    public class CiudadesController : Controller
+    {
+        private readonly AplicationDbContext _context;
+
+        public CiudadesController(AplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Ciudades
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.ciudades.ToListAsync());
+        }
+
+        // GET: Ciudades/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ciudades == null)
+            {
+                return NotFound();
+            }
+
+            var ciudades = await _context.ciudades
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ciudades == null)
+            {
+                return NotFound();
+            }
+
+            return View(ciudades);
+        }
+
+        // GET: Ciudades/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ciudades/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nombre")] Ciudades ciudades)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ciudades);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ciudades);
+        }
+
+        // GET: Ciudades/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.ciudades == null)
+            {
+                return NotFound();
+            }
+
+            var ciudades = await _context.ciudades.FindAsync(id);
+            if (ciudades == null)
+            {
+                return NotFound();
+            }
+            return View(ciudades);
+        }
+
+        // POST: Ciudades/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre")] Ciudades ciudades)
+        {
+            if (id != ciudades.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ciudades);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CiudadesExists(ciudades.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ciudades);
+        }
+
+        // GET: Ciudades/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.ciudades == null)
+            {
+                return NotFound();
+            }
+
+            var ciudades = await _context.ciudades
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (ciudades == null)
+            {
+                return NotFound();
+            }
+
+            return View(ciudades);
+        }
+
+        // POST: Ciudades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.ciudades == null)
+            {
+                return Problem("Entity set 'AplicationDbContext.ciudades'  is null.");
+            }
+            var ciudades = await _context.ciudades.FindAsync(id);
+            if (ciudades != null)
+            {
+                _context.ciudades.Remove(ciudades);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CiudadesExists(int id)
+        {
+          return _context.ciudades.Any(e => e.Id == id);
+        }
+    }
+}
